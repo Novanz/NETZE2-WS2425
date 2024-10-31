29990f84b3ec7be046f1254956f6f0b164e3e4f5
Novan@820 <55051643+Novanz@users.noreply.github.com>
2024-10-31 08:43:20 +0100

    working version

--- B3/SimpleHTTPServer.java
+++ B3/SimpleHTTPServer.java
@@ -4,11 +4,14 @@ import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+//TODO: remove debug output, comment code, add comments, add max-age to cookie
+
 public class SimpleHTTPServer {
     private static Date parseCookie(String request) {
         System.out.println("in parseCookie...");
         String cookiePrefix = "Last-Access=";
         int startIndex = request.indexOf(cookiePrefix);
+        System.out.println("startIndex: " + startIndex);
         if (startIndex == -1) {
             return null;
         }
@@ -18,23 +21,26 @@ public class SimpleHTTPServer {
             endIndex = request.length();
         }
         String dateString = request.substring(startIndex, endIndex);
+        System.out.println("dateString: " + dateString);
         try {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(dateString);
+            return new SimpleDateFormat("dd.MM.yyyy, HH:mm:ss").parse(dateString);
         } catch (Exception e) {
+            System.out.println("Fehler beim Parsen des Cookies");
             return null;
         }
     }
 
+    // extractt html generattor
     private static String generateResponse(Date lastAccessTimestamp) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy, HH:mm");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy, HH:mm:ss");
         String currentTime = sdf.format(new Date());
         StringBuilder response = new StringBuilder();
         // HTTP-Header
-        response.append("HTTP/1.1 200 OK\r\n\r\n");
+        response.append("HTTP/1.1 200 OK\r\n");
         response.append("Content-Type: text/html\r\n");
-        response.append("Set-Cookie: lastVisit=").append(currentTime)
-                .append("; Max-Age=3600\r\n");
-        response.append("\r\n");
+        response.append("Set-Cookie: Last-Access=").append(currentTime);
+        //  .append("; Max-Age=3600\r\n");
+        response.append("\r\n\n");
         // HTML-Body
         response.append("<!DOCTYPE html><html><body>");
         response.append("<h2>Hallo! Es ist der ").append(currentTime).append(" Uhr.</h2>");
@@ -51,7 +57,6 @@ public class SimpleHTTPServer {
     }
 
     private static String readText(Socket s) throws IOException {
-        //PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
         BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
         String str;
         StringBuilder sb = new StringBuilder();
@@ -63,7 +68,7 @@ public class SimpleHTTPServer {
             sb.append(str);
             sb.append("\r\n");
         }
-        input.close();
+        //input.close();
         System.out.println("end of while loop");
         return (sb.toString());
     }
@@ -71,6 +76,7 @@ public class SimpleHTTPServer {
     private static void sendText(Socket s, String string) {
         System.out.println("Durch Socket uebertragen...");
         try {
+            System.out.println(string);
             OutputStreamWriter osw = new OutputStreamWriter(s.getOutputStream());
             osw.write(string);
             osw.flush();
